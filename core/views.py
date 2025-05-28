a5cb1b1906ce7bf6af598f122bf3fb935c0b88f5
harshadakhorgade <harshadakhorgade9@gmail.com>
2025-05-28 16:42:44 +0530

    start again CI/CD

--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("✅ Hello from Dockerized Django appliaction  ")
+    return HttpResponse("✅ Hello from Dockerized Django !!  ")
